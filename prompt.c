34bf51188df6a82bb1116196422a27a740286d7d
Wafaa <wafaagaafar@gmail.com>
2023-10-07 16:52:57 +0200

    updates

--- prompt.c
+++
@@ -1,6 +0,0 @@
-#include "shell.h"
-
-void prompt(void)
-{
-    printf("simple_shell$$ ");
-}
--- shell.h
+++ shell.h
@@ -1,13 +1,4 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-void prompt();
-void input(char *command, size_t size);
-void execute(char command);
-
 #endif
