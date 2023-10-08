f8b11db8cb1559f0c77b36869197404db8b55564
Batman99SD <azozkrg1@gmail.com>
2023-10-08 21:13:11 +0300

    azizmake some changes

--- main.c
+++ main.c
@@ -3,15 +3,18 @@
 int main(int ac, char **argv)
 {
     char *prompt = "The_shell_is_this $";
-    char *linptr;
+    char *linptr = NULL, *cp_linptr = NULL;
     size_t n = 0;
     ssize_t stread;
+    const char *delim = " ";
+    int num_pars = 0;
+    char *pars;
+    int i;
 
     (void)ac;
 
     while (prompt)
     {
-        int i;
 
         write(1, prompt, strlen(prompt));
         stread = getline(&linptr, &n, stdin);
@@ -21,10 +24,47 @@ int main(int ac, char **argv)
             write(1, "\n", 1);
             break;
         }
-        for (i = 0; argv[i]; i++)
+
+        /*allocate space for copy of cp_linptr*/
+        cp_linptr = malloc(sizeof(char) * stread);
+        if (stread == -1)
         {
-            printf("%s: No such file or directory\n", argv[i]);
+            write(1, "tsh: memory allocation error", 28);
+            return (-1);
         }
+
+        /*copy linptr to cp_linptr*/
+        strcpy(cp_linptr, linptr);
+
+        /*split linptr into array of words*/
+        /*calculate number of tokens*/
+        pars = strtok(linptr, delim);
+
+        while (pars != NULL)
+        {
+            num_pars++;
+            pars = strtok(NULL, delim);
+        }
+        num_pars++;
+
+        /*allocate space to hold the array of strings*/
+        argv = malloc(sizeof(char) * num_pars);
+        /*store the pars in the argv array*/
+        pars = strtok(cp_linptr, delim);
+
+        for (i = 0; pars != NULL; i++)
+        {
+            argv[i] = malloc(sizeof(char) * strlen(pars));
+            
+            strcpy(argv[i], pars);
+            pars = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+        write(1, linptr, strlen(linptr));
+
     }
+    free(linptr);
+
     return (0);
 }
