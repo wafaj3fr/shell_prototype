351028931182738be5a6d9fd7af63552b46de299
Wafaa <wafaagaafar@gmail.com>
2023-10-09 18:47:53 +0200

    edited mod.c

--- mod.c
+++ mod.c
@@ -5,20 +5,24 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
-void execcom(char **argv) {
+void execcom(char **argv)
+{
     char *com = NULL;
 
-    if (argv) {
+    if (argv)
+    {
         com = argv[0];
 
-        if (execve(com, argv, NULL) == -1) {
+        if (execve(com, argv, NULL) == -1)
+        {
             perror("Error");
             exit(EXIT_FAILURE); // Exit the child process on execve error
         }
     }
 }
 
-int main(int ac, char **argv) {
+int main(int ac, char **argv)
+{
     char *prompt = "The_shell_is_this $";
     char *linptr = NULL, *cp_linptr = NULL;
     size_t n = 0;
@@ -31,15 +35,20 @@ int main(int ac, char **argv) {
 
     (void)ac;
 
-    while (1) { // Infinite loop
+    while (1)
+    { // Infinite loop
         write(1, prompt, strlen(prompt));
         stread = getline(&linptr, &n, stdin);
 
-        if (stread == -1) {
-            if (feof(stdin)) { // Check for EOF (Ctrl+D)
+        if (stread == -1)
+        {
+            if (feof(stdin))
+            { // Check for EOF (Ctrl+D)
                 printf("\nExiting shell...\n");
                 break; // Exit the loop gracefully
-            } else {
+            }
+            else
+            {
                 perror("getline");
                 exit(EXIT_FAILURE);
             }
@@ -47,13 +56,65 @@ int main(int ac, char **argv) {
 
         /* The rest of your code remains the same */
 
-        // Your code to tokenize the input and fork/exec
+        /*allocate space for copy of cp_linptr*/
+        cp_linptr = malloc(sizeof(char) * stread);
+        if (cp_linptr == NULL)
+        {
+            write(1, "tsh: memory allocation error", 28);
+            return (-1);
+        }
 
-        /* Free allocated memory */
-        free(cp_linptr);
-        free(linptr);
-        // Free argv array memory here (if you malloc it), before reassigning it
+        /*copy linptr to cp_linptr*/
+        strcpy(cp_linptr, linptr);
+
+        /*split linptr into array of words*/
+        /*calculate number of tokens*/
+        pars = strtok(linptr, delim);
+
+        while (pars != NULL)
+        {
+            num_pars++;
+            pars = strtok(NULL, delim);
+        }
+        num_pars++;
+
+        /*allocate space to hold the array of strings*/
+        argv = malloc(sizeof(char *) * num_pars);
+        /*store the pars in the argv array*/
+        pars = strtok(cp_linptr, delim);
+
+        for (i = 0; pars != NULL; i++)
+        {
+            argv[i] = malloc(sizeof(char) * strlen(pars));
+
+            strcpy(argv[i], pars);
+            pars = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+        pid = fork();
+
+        if (pid == -1)
+        {
+            perror("fork");
+            return (-1);
+        }
+        else if (pid == 0)
+        {
+
+            execcom(argv);
+        }
+        else
+        {
+            int status;
+            wait(&status);
+        }
     }
+
+    /* Free allocated memory */
+    free(cp_linptr);
+    free(linptr);
+    // Free argv array memory here (if you malloc it), before reassigning it
 
     return 0;
 }
