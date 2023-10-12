ef5e63398adf5598a26992169905df0fe5e7a821
Wafaa <wafaagaafar@gmail.com>
2023-10-12 10:31:48 +0200

    added a directory

---
+++ obeds/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+char *get_location(char *command);
+void execmd(char **argv);
+
+#endif
