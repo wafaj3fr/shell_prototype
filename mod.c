a21e87c71b17637f5107f93d8e012b1d55163701
Batman99SD <azozkrg1@gmail.com>
2023-10-14 19:56:13 +0300

    the old mod

--- mod.c
+++ mod.c
@@ -1,13 +1,8 @@
 #include "main.h"
 
-/**
- * execom - executes the command
- * @argv: the command to be executed
- */
-
 void execcom(char **argv)
 {
-    char *com;
+    char *com = NULL;
 
     if (argv)
     {
@@ -16,21 +11,14 @@ void execcom(char **argv)
         if (execve(com, argv, NULL) == -1)
         {
             perror("Error");
-            exit(EXIT_FAILURE);
+            exit(EXIT_FAILURE); // Exit the child process on execve error
         }
     }
 }
 
-/**
- * main - Entry point
- * @ac: arguments count
- * @argv: arguments
- * Return: 0 for success
- */
-
-int r(int ac, char **argv)
+int main(int ac, char **argv)
 {
-    char *prompt = "ay $";
+    char *prompt = "The_shell_is_this $";
     char *linptr = NULL, *cp_linptr = NULL;
     size_t n = 0;
     ssize_t stread;
@@ -42,86 +30,80 @@ int r(int ac, char **argv)
 
     (void)ac;
 
-    while (prompt)
+    while (1)
     { // Infinite loop
         write(1, prompt, strlen(prompt));
         stread = getline(&linptr, &n, stdin);
 
         if (stread == -1)
         {
-            if (feof(stdin))
-            {
-                write(1, "\n", 1);
-                break;
-                if (linptr != NULL && linptr[0] == '\0')
-                { // Check if an empty line was entered (Ctrl+D)
-                    printf("\nExiting shell...\n");
-                    break; // Exit the loop gracefully
-                }
-                else
-                {
-                    perror("getline");
-                    exit(EXIT_FAILURE);
-                }
-            }
-
-            /*allocate space for copy of cp_linptr*/
-            cp_linptr = malloc(sizeof(char) * stread);
-            if (cp_linptr == NULL)
-            {
-                write(1, "tsh: memory allocation error", 28);
-                return (-1);
-            }
-
-            /*copy linptr to cp_linptr*/
-            strcpy(cp_linptr, linptr);
-
-            /*split linptr into array of words*/
-            /*calculate number of tokens*/
-            pars = strtok(linptr, delim);
-
-            while (pars != NULL)
-            {
-                num_pars++;
-                pars = strtok(NULL, delim);
-            }
-            num_pars++;
-
-            /*allocate space to hold the array of strings*/
-            argv = malloc(sizeof(char *) * num_pars);
-            /*store the pars in the argv array*/
-            pars = strtok(cp_linptr, delim);
-
-            for (i = 0; pars != NULL; i++)
-            {
-                argv[i] = malloc(sizeof(char) * strlen(pars));
-
-                strcpy(argv[i], pars);
-                pars = strtok(NULL, delim);
-            }
-            argv[i] = NULL;
-
-            pid = fork();
-
-            if (pid == -1)
-            {
-                perror("fork");
-                return (-1);
-            }
-            else if (pid == 0)
-            {
-
-                execcom(argv);
+            if (linptr != NULL && linptr[0] == '\0') { // Check if an empty line was entered (Ctrl+D)
+                printf("\nExiting shell...\n");
+                break; // Exit the loop gracefully
             }
             else
             {
-                int status;
-                wait(&status);
+                perror("getline");
+                exit(EXIT_FAILURE);
             }
         }
 
-        free(cp_linptr);
-        free(linptr);
+        /*allocate space for copy of cp_linptr*/
+        cp_linptr = malloc(sizeof(char) * stread);
+        if (cp_linptr == NULL)
+        {
+            write(1, "tsh: memory allocation error", 28);
+            return (-1);
+        }
+
+        /*copy linptr to cp_linptr*/
+        strcpy(cp_linptr, linptr);
+
+        /*split linptr into array of words*/
+        /*calculate number of tokens*/
+        pars = strtok(linptr, delim);
+
+        while (pars != NULL)
+        {
+            num_pars++;
+            pars = strtok(NULL, delim);
+        }
+        num_pars++;
+
+        /*allocate space to hold the array of strings*/
+        argv = malloc(sizeof(char *) * num_pars);
+        /*store the pars in the argv array*/
+        pars = strtok(cp_linptr, delim);
+
+        for (i = 0; pars != NULL; i++)
+        {
+            argv[i] = malloc(sizeof(char) * strlen(pars));
+
+            strcpy(argv[i], pars);
+            pars = strtok(NULL, delim);
+        }
+        argv[i] = NULL;
+
+        pid = fork();
+
+        if (pid == -1)
+        {
+            perror("fork");
+            return (-1);
+        }
+        else if (pid == 0)
+        {
+
+            execcom(argv);
+        }
+        else
+        {
+            int status;
+            wait(&status);
+        }
     }
+
+    free(cp_linptr);
+    free(linptr);
     return 0;
 }
