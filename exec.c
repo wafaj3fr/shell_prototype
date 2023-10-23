f1137eb390bb18f04bebcf3222aabba7b0f0f1ba
Batman99SD <127743755+Batman99SD@users.noreply.github.com>
2023-10-23 23:19:16 +0300

    Update exec.c

--- exec.c
+++ exec.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * execmd - executes the command
+ * execute - executes the command
  * @argv: the command to be executed
+ * @path: the path
  */
 
 void execute(char *const *argv, char *path)
 {
 
-    if (execve(path, argv, NULL) == -1)
-    {
-        perror("Error:");
-    }
+	if (execve(path, argv, NULL) == -1)
+	{
+		perror("Error:");
+	}
 }
