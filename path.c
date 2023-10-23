9c453fd1188c0483a79be375560e0a1ce6da35e7
Batman99SD <127743755+Batman99SD@users.noreply.github.com>
2023-10-23 23:20:48 +0300

    Update path.c

--- path.c
+++ path.c
@@ -1,87 +1,103 @@
 #include "main.h"
-
+/**
+ * find_exe - find exe file if found.
+ * @command: exe file name or path.
+ * Return: NULL if fail, full_path if seccuse.
+ */
 char *find_exe(char *command)
 {
-    char *path, *path_copy, *dir, *full_path;
-    int found = 0;
-    size_t max_path_length = 1024;
+	char *path, *path_copy, *dir, *full_path;
+	int found = 0;
+	size_t max_path_length = 1024;
 
-    if (command[0] == '/')
-    {
-        full_path = command;
-        return (full_path);
-    }
+	if (command[0] == '/')
+	{
+		full_path = command;
+		return (full_path);
+	}
 
-    path = getenv("PATH");
-    if (path == NULL)
-    {
-        return NULL;
-    }
+	while (*command == ' ' || *command == '\t')
+	{
+		command++;
+	}
+	path = getenv("PATH");
+	if (path == NULL)
+	{
+		return (NULL);
+	}
 
-    path_copy = strdup(path);
-    dir = strtok(path_copy, ":");
+	path_copy = strdup(path);
+	dir = strtok(path_copy, ":");
 
-    full_path = malloc(max_path_length);
-    if (full_path == NULL)
-    {
-        free(path_copy);
-        free(full_path);
-        return NULL;
-    }
+	full_path = malloc(max_path_length);
+	if (full_path == NULL)
+	{
+		free(path_copy);
+		free(full_path);
+		return (NULL);
+	}
 
-    while (dir != NULL)
-    {
-        snprintf(full_path, max_path_length, "%s/%s", dir, command);
+	while (dir != NULL)
+	{
+		snprintf(full_path, max_path_length, "%s/%s", dir, command);
 
-        if (strlen(full_path) >= max_path_length)
-        {
-            max_path_length *= 2;
-            free(full_path);
-            full_path = malloc(max_path_length);
-            if (full_path == NULL)
-            {
-                free(path_copy);
-                free(full_path);
-                return NULL;
-            }
-        }
+		if (strlen(full_path) >= max_path_length)
+		{
+			max_path_length *= 2;
+			free(full_path);
+			full_path = malloc(max_path_length);
+			if (full_path == NULL)
+			{
+				free(path_copy);
+				free(full_path);
+				return (NULL);
+			}
+		}
 
-        if (access(full_path, X_OK) == 0)
-        {
-            found = 1;
-            break;
-        }
-        dir = strtok(NULL, ":");
-    }
+		if (access(full_path, X_OK) == 0)
+		{
+			found = 1;
+			break;
+		}
+		dir = strtok(NULL, ":");
+	}
 
-    free(path_copy);
+	free(path_copy);
 
-    if (found)
-    {
-        return full_path;
-    }
-    else
-    {
-        free(full_path);
-        return NULL;
-    }
+	if (found)
+	{
+		return (full_path);
+	}
+	else
+	{
+		free(full_path);
+		return (NULL);
+	}
 }
-
-void print_environment()
+/**
+ * print_environment - prints the env var.
+ * Return: NULL.
+ */
+void print_environment(void)
 {
-    extern char **environ;
+	extern char **environ;
 
-    char **env = environ;
-    while (*env) {
-        write(1, *env, _strlen(*env));
-        write(1, "\n", 2);
-        env++;
-    }
+	char **env = environ;
+	while (*env)
+	{
+		write(1, *env, _strlen(*env));
+		write(1, "\n", 2);
+		env++;
+	}
 }
 
+/**
+ * dollar_prpmpt - prints the prompt.
+ * Return: NULL.
+ */
 void dollar_prpmpt(void)
 {
-    char *prompt = "$ ";
+	char *prompt = "$ ";
 
-    write(1, prompt, _strlen(prompt));
+	write(1, prompt, _strlen(prompt));
 }
